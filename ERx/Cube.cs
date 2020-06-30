32d3761b1b4a8f67146361c723376476147663c1
gami0512 <DragonGodGM@users.noreply.github.com>
2020-06-30 17:44:57 +0900

    .

---
+++ ERx/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Ex07_Class
+{
+    internal class Cube
+    {
+        int x, y, z;
+        static int countOfInstance = 0;
+        public Cube(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            countOfInstance++;
+        }
+        public int getVolume()
+        {
+            System.Console.WriteLine($"생성된 큐브의 갯수 : {countOfInstance}");
+            return x * y * z;
+        }
+    }
+}
---
+++ ERx/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cube cube1 = new Cube(1,2,3);
+            Console.WriteLine($"cube1의 부피 : {cube1.getVolume()}");
+        }
+    }
+}
